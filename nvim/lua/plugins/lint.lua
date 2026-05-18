f322bbe53a7baf99359a17d07bc5de58102fc746
jefferson oliveira <jeffersonoliveiradev@gmail.com>
2026-05-18 02:18:08 -0300

    update

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -3,15 +3,29 @@ if not ok then
 	return
 end
 
-lint.linters_by_ft = {
+local function has(bin)
+	return vim.fn.executable(bin) == 1
+end
+
+local linters_by_ft = {
 	javascript = { "eslint_d" },
 	javascriptreact = { "eslint_d" },
 	typescript = { "eslint_d" },
 	typescriptreact = { "eslint_d" },
-	go = { "golangcilint" },
 	rust = { "clippy" },
 	java = { "checkstyle" },
 }
+
+-- golangci-lint is optional; without it nvim-lint raises ENOENT on every :w
+if has("golangci-lint") then
+	linters_by_ft.go = { "golangcilint" }
+elseif has("staticcheck") then
+	linters_by_ft.go = { "staticcheck" }
+elseif has("revive") then
+	linters_by_ft.go = { "revive" }
+end
+
+lint.linters_by_ft = linters_by_ft
 
 local group = vim.api.nvim_create_augroup("nvim_lint", { clear = true })
 vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
